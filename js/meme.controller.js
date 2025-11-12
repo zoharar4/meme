861e8f5fa438bc8299f69edee57f6dd189e27047
Zohar <zoharar4@gmail.com>
2025-11-12 15:25:43 +0200

    fonts & gallery

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -2,12 +2,12 @@
 
 
 function onInit() {
-    createGallertImgs()
+    renderMemeGallery()
 }
 
-function createGallertImgs() {
+function renderMemeGallery() {
     const imgs = getImgsArray()
     const gallery = document.querySelector('.gallery-imgs')
-    var strHTML = imgs.map(img => `<img src="${img.url}"></img>`).join("")
+    var strHTML = imgs.map(img => `<img id="${img.id}" class="gallery-img" src="${img.url}"></img>`).join("")
     gallery.innerHTML = strHTML
 }
