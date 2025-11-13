b4fba627811117c2a90bc046dc0244177680386d
Zohar <zoharar4@gmail.com>
2025-11-13 14:00:47 +0200

    edit

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,24 +1,82 @@
 const gElCanvas = document.querySelector('canvas')
 const gCtx = gElCanvas.getContext('2d')
-
+const fontSizeStep = 2
+var gLineIdx = 0
 
 function onInit() {
     renderMemeGallery()
+    renderMeme()
+
 }
 
-function renderMemeGallery() {
+function renderMemeGallery() {  //renders the gallery
     const imgs = getImgsArray()
     const gallery = document.querySelector('.gallery-imgs')
-    var strHTML = imgs.map(img => `<img id="${img.id}" class="gallery-img" src="${img.url}"></img>`).join("")
+    var strHTML = imgs.map(img => `<img onclick="onClickedImg(${img.id})" class="gallery-img" src="${img.url}"></img>`).join("")
     gallery.innerHTML = strHTML
 }
 
+function onClickedImg(id) {     //in the gallery
+    createGMeme(id)      // create the gMeme
+    renderMeme()
+}
 
-renderMeme()
-function renderMeme() {
+function renderMeme() {   //renders the curr meme in the canvas
     const meme = getMeme()
     const img = new Image()
     img.src = meme.imgUrl
-    img.onload = () => gCtx.drawImage(img,0,0,gElCanvas.width,gElCanvas.height)
-    console.log('1:',1)
+
+    img.onload = () => {
+        gElCanvas.height = (img.naturalHeight / img.naturalWidth) * gElCanvas.width
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+
+        meme.lines.forEach((line, idx) => drawText({ idx, ...line }))
+    }
+}
+
+function drawText({ idx, txt, size, color, fontFamily, txtAlign, x, y }) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = color
+    gCtx.font = `${size}px ${fontFamily}`
+    gCtx.textAlign = txtAlign
+    gCtx.textBaseline = 'top'
+    gCtx.lineJoin = 'round'
+    gCtx.fillText(txt, x, y)
+    gCtx.strokeText(txt, x, y)
+
+    if (idx === getMeme().selectedLineIdx) {
+        //todo - textBox
+    }
+}
+
+
+function onTextChanged(txt) {
+    changeText(txt)
+    renderMeme()
+}
+function onFontSize(action) {
+    fontSize(action)
+    renderMeme()
+}
+function onTextAlign(direction) {
+    alignText(direction)
+    renderMeme()
+}
+// function onTextChanged(txt) {
+//     changeText(txt)
+//     renderMeme()
+// }
+// function onTextChanged(txt) {
+//     changeText(txt)
+//     renderMeme()
+// }
+
+
+function onDownloadCanvas() {
+    const link = document.createElement("a")
+    const url = gElCanvas.toDataURL()
+    link.href = url
+    link.download = 'image'
+    link.click()
 }
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -12,17 +12,30 @@ var gImgs = [
 var gMeme = {
     selectedImgId: 5,
     selectedLineIdx: 0,
-    imgUrl: "/images/meme-square/7.jpg",
+    imgUrl: "/images/meme-square/7.jpg", //temporery
     lines: [
         {
             txt: 'I sometimes eat Falafel',
-            size: 20,
-            color: 'red'
+            size: 60,
+            color: 'rgb(30,100,50)',
+            fontFamily: 'impact',
+            txtAlign: 'center',
+            x: 300,
+            y: 300,
+        },
+        {
+            txt: 'I hhhhhhhhhsometimes eat Falafel',
+            size: 30,
+            color: 'rgb(30,100,50)',
+            fontFamily: 'impact',
+            txtAlign: 'center',
+            x: gElCanvas.width / 2,
+            y: gElCanvas.height / 2,
         }
     ]
 }
-var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 
+var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 
 function getImgsArray() {
     return gImgs
@@ -30,4 +43,43 @@ function getImgsArray() {
 
 function getMeme() {
     return gMeme
+}
+
+function createGMeme(id) {  //creates the gMeme with default values when img clicked 
+    var currImg = gImgs.find(img => img.id === id)
+    gMeme = {
+        selectedImgId: id,
+        selectedLineIdx: 0,
+        imgUrl: currImg.url,
+        lines: [
+            {
+                txt: 'some text',
+                size: 30,
+                color: 'rgb(30,100,50)',
+                fontFamily: 'impact',
+                txtAlign: 'center',
+                x: gElCanvas.width / 2,
+                y: gElCanvas.height / 2,
+            },
+        ]
+    }
+    console.log('gMeme:', gMeme)
+}
+
+function changeText(txt) {
+    gMeme.lines[gMeme.selectedLineIdx].txt = txt
+}
+
+function fontSize(action) {
+    if (action === 'increase') gMeme.lines[gMeme.selectedLineIdx].size += fontSizeStep
+    else gMeme.lines[gMeme.selectedLineIdx].size -= fontSizeStep
+}
+
+function alignText(direction) {
+    var xPos = gElCanvas.width/2
+    if (direction === 'left') xPos = 5
+    else if (direction === 'right') xPos = gElCanvas.width - 5
+
+    gMeme.lines[gMeme.selectedLineIdx].x = xPos
+    gMeme.lines[gMeme.selectedLineIdx].txtAlign = direction
 }
