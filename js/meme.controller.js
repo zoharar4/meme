bbf9c8a561a2bb0744765300708af19bfee7b075
Zohar <zoharar4@gmail.com>
2025-11-13 17:33:16 +0200

    edit functions

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,7 +1,8 @@
 const gElCanvas = document.querySelector('canvas')
 const gCtx = gElCanvas.getContext('2d')
 const fontSizeStep = 2
-var gLineIdx = 0
+const moveStep = 10
+var downloading = false
 
 function onInit() {
     renderMemeGallery()
@@ -25,6 +26,12 @@ function renderMeme() {   //renders the curr meme in the canvas
     const meme = getMeme()
     const img = new Image()
     img.src = meme.imgUrl
+    
+    if (!meme.lines.length) {
+        addText()
+    }
+    
+    document.querySelector('#memeTextInput').value = meme.lines[meme.selectedLineIdx].txt
 
     img.onload = () => {
         gElCanvas.height = (img.naturalHeight / img.naturalWidth) * gElCanvas.width
@@ -36,17 +43,27 @@ function renderMeme() {   //renders the curr meme in the canvas
 
 function drawText({ idx, txt, size, color, fontFamily, txtAlign, x, y }) {
     gCtx.lineWidth = 2
-    gCtx.strokeStyle = 'black'
+
     gCtx.fillStyle = color
     gCtx.font = `${size}px ${fontFamily}`
     gCtx.textAlign = txtAlign
     gCtx.textBaseline = 'top'
     gCtx.lineJoin = 'round'
+    gCtx.setLineDash([])
     gCtx.fillText(txt, x, y)
     gCtx.strokeText(txt, x, y)
 
-    if (idx === getMeme().selectedLineIdx) {
-        //todo - textBox
+    if (idx === getMeme().selectedLineIdx && !downloading) {
+        const textWidth = gCtx.measureText(txt).width
+        const textHeight = size
+        var rectX = x
+        if (txtAlign === 'center') rectX = x - textWidth / 2
+        else if (txtAlign === 'right') rectX = x - textWidth
+        var rectY = y
+        gCtx.strokeStyle = 'black'
+        gCtx.lineWidth = 3
+        gCtx.setLineDash([6, 4])
+        gCtx.strokeRect(rectX - 5, rectY - 5, textWidth + 10, textHeight + 10)
     }
 }
 
@@ -55,28 +72,64 @@ function onTextChanged(txt) {
     changeText(txt)
     renderMeme()
 }
+
+function onMoveLine(direction) {
+    if (direction === 'up') getMeme().lines[getMeme().selectedLineIdx].y -= moveStep
+    else getMeme().lines[getMeme().selectedLineIdx].y += moveStep
+
+    renderMeme()
+}
+
+function onDeleteLine() {
+    getMeme().lines.splice(getMeme().selectedLineIdx,1)
+    getMeme().selectedLineIdx = 0
+    renderMeme()
+    console.log('getMeme():',getMeme())
+}
+
 function onFontSize(action) {
     fontSize(action)
     renderMeme()
 }
+
 function onTextAlign(direction) {
     alignText(direction)
     renderMeme()
 }
-// function onTextChanged(txt) {
-//     changeText(txt)
-//     renderMeme()
-// }
-// function onTextChanged(txt) {
-//     changeText(txt)
-//     renderMeme()
-// }
 
+function onChangeLine() {
+    changeLineIdx()
+    renderMeme()
+}
+
+function onAddText() {
+    addText()
+    renderMeme()
+}
+
+function onColorBtn() {
+    document.querySelector('.inputColorText').click()
+}
+
+function onChangedColor(value) {
+    getMeme().lines[getMeme().selectedLineIdx].color = value
+    renderMeme()
+}
+
+function onAddText() {
+    addText()
+}
 
 function onDownloadCanvas() {
-    const link = document.createElement("a")
-    const url = gElCanvas.toDataURL()
-    link.href = url
-    link.download = 'image'
-    link.click()
+    downloading = true
+    renderMeme()
+    setTimeout(function () {
+        const link = document.createElement("a")
+        const url = gElCanvas.toDataURL()
+        link.href = url
+        link.download = 'image'
+        link.click()
+        downloading = false
+        renderMeme()
+    }, 1000)
 }
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -5,8 +5,34 @@ var gImgs = [
     { id: 4, url: "images/meme-square/4.jpg", keywords: ['funny', 'cat'] },
     { id: 5, url: "images/meme-square/5.jpg", keywords: ['funny', 'cat'] },
     { id: 6, url: "images/meme-square/6.jpg", keywords: ['funny', 'cat'] },
-    { id: 7, url: "images/meme-square/7.jpg", keywords: ['funny', 'cat'] },
-
+    { id: 7, url: "images/meme-square/18.jpg", keywords: ['funny', 'cat'] },
+    { id: 8, url: "images/meme-square/8.jpg", keywords: ['funny', 'cat'] },
+    { id: 9, url: "images/meme-square/9.jpg", keywords: ['funny', 'cat'] },
+    { id: 10, url: "images/meme-square/10.jpg", keywords: ['funny', 'cat'] },
+    { id: 11, url: "images/meme-square/11.jpg", keywords: ['funny', 'cat'] },
+    { id: 12, url: "images/meme-square/12.jpg", keywords: ['funny', 'cat'] },
+    { id: 13, url: "images/meme-square/13.jpg", keywords: ['funny', 'cat'] },
+    { id: 14, url: "images/meme-square/14.jpg", keywords: ['funny', 'cat'] },
+    { id: 15, url: "images/meme-square/15.jpg", keywords: ['funny', 'cat'] },
+    { id: 16, url: "images/meme-square/16.jpg", keywords: ['funny', 'cat'] },
+    { id: 18, url: "images/meme-imgs-2/18.jpg", keywords: ['funny', 'cat'] },
+    { id: 17, url: "images/meme-square/17.jpg", keywords: ['funny', 'cat'] },
+    { id: 19, url: "images/meme-imgs-2/1.jpg", keywords: ['funny', 'trump'] },
+    { id: 20, url: "images/meme-imgs-2/2.jpg", keywords: ['funny', 'animal'] },
+    { id: 21, url: "images/meme-imgs-2/3.jpg", keywords: ['baby', 'cat'] },
+    { id: 22, url: "images/meme-imgs-2/4.jpg", keywords: ['funny', 'cat'] },
+    { id: 23, url: "images/meme-imgs-2/5.jpg", keywords: ['funny', 'cat'] },
+    { id: 24, url: "images/meme-imgs-2/6.jpg", keywords: ['funny', 'cat'] },
+    { id: 25, url: "images/meme-imgs-2/8.jpg", keywords: ['funny', 'cat'] },
+    { id: 26, url: "images/meme-imgs-2/9.jpg", keywords: ['funny', 'cat'] },
+    { id: 27, url: "images/meme-imgs-2/10.jpg", keywords: ['funny', 'cat'] },
+    { id: 28, url: "images/meme-imgs-2/11.jpg", keywords: ['funny', 'cat'] },
+    { id: 29, url: "images/meme-imgs-2/12.jpg", keywords: ['funny', 'cat'] },
+    { id: 30, url: "images/meme-imgs-2/13.jpg", keywords: ['funny', 'cat'] },
+    { id: 31, url: "images/meme-imgs-2/14.jpg", keywords: ['funny', 'cat'] },
+    { id: 32, url: "images/meme-imgs-2/15.jpg", keywords: ['funny', 'cat'] },
+    { id: 33, url: "images/meme-imgs-2/16.jpg", keywords: ['funny', 'cat'] },
+    { id: 34, url: "images/meme-imgs-2/17.jpg", keywords: ['funny', 'cat'] },
 ]
 
 var gMeme = {
@@ -16,7 +42,7 @@ var gMeme = {
     lines: [
         {
             txt: 'I sometimes eat Falafel',
-            size: 60,
+            size: 40,
             color: 'rgb(30,100,50)',
             fontFamily: 'impact',
             txtAlign: 'center',
@@ -52,15 +78,7 @@ function createGMeme(id) {  //creates the gMeme with default values when img cli
         selectedLineIdx: 0,
         imgUrl: currImg.url,
         lines: [
-            {
-                txt: 'some text',
-                size: 30,
-                color: 'rgb(30,100,50)',
-                fontFamily: 'impact',
-                txtAlign: 'center',
-                x: gElCanvas.width / 2,
-                y: gElCanvas.height / 2,
-            },
+            getLine()
         ]
     }
     console.log('gMeme:', gMeme)
@@ -76,10 +94,45 @@ function fontSize(action) {
 }
 
 function alignText(direction) {
-    var xPos = gElCanvas.width/2
+    var xPos = gElCanvas.width / 2
     if (direction === 'left') xPos = 5
     else if (direction === 'right') xPos = gElCanvas.width - 5
 
     gMeme.lines[gMeme.selectedLineIdx].x = xPos
     gMeme.lines[gMeme.selectedLineIdx].txtAlign = direction
+}
+
+function changeLineIdx() {
+    const currLineIdx = gMeme.selectedLineIdx
+    const linesLength = gMeme.lines.length
+
+    if (linesLength <= 1) return
+    if (linesLength === (currLineIdx + 1)) {
+        console.log('1:', 1)
+        gMeme.selectedLineIdx = 0
+        console.log('gMeme.selectedLineIdx:', gMeme.selectedLineIdx)
+    } else {
+        console.log('gMeme.selectedLineIdx:', gMeme.selectedLineIdx)
+        gMeme.selectedLineIdx += 1
+    }
+}
+
+function addText() {
+    const newText = getLine()
+    gMeme.lines.push(newText)
+    gMeme.selectedLineIdx = gMeme.lines.length - 1
+    renderMeme()
+}
+
+
+function getLine() {
+    return {
+        txt: 'text',
+        size: 35,
+        color: 'rgba(18, 153, 130, 1)',
+        fontFamily: 'impact',
+        txtAlign: 'center',
+        x: gElCanvas.width / 2,
+        y: gElCanvas.height / 5,
+    }
 }
